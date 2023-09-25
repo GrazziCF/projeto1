b7d00eef0bb6af6d331cad1e1b77f9b1f06102c9
GrazziCF <141650133+GrazziCF@users.noreply.github.com>
2023-09-24 21:45:29 -0400

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,41 @@
+export default [{
+        src: "./musicas/seven.mp3",
+        name: "Seven - Jeon JungKook",
+    },
+    {
+        src: "./musicas/fever.mp3",
+        name: "Fever - Enhypen",
+    },
+    {
+        src: "./musicas/mirros.mp3",
+        name: "Mirrors - Justin Timberlake",
+    },
+    {
+        src: "./musicas/thehills.mp3",
+        name: "The Hills x The Color Violet x Creepin",
+    },
+    {
+        src: "./musicas/goldenhour.mp3",
+        name: "Golden Hour - JVKE",
+    },
+    {
+        src: "./musicas/daylight.mp3",
+        name: "Daylight -  David Kushner",
+    },
+    {
+        src: "./musicas/congratulations.mp3",
+        name: "Congratulations -  Post Malone",
+    },
+    {
+        src: "./musicas/lastfriday.mp3",
+        name: "Last Friday Night - Katy Perry",
+    },
+    {
+        src: "./musicas/leehong.mp3",
+        name: "Still Love You -  Lee Hong Gi",
+    },
+    {
+        src: "./musicas/strangervibes.mp3",
+        name: "Strange Vibes - DJ Boss",
+    },
+];
